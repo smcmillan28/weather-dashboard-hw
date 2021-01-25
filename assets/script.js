7ee42df2bc3b30744d8fda3707a29d2d75bfef18
Sammy McMillan <74073409+smcmillan28@users.noreply.github.com>
2021-01-25 17:20:05 -0500

    edited styling, confirmed jQuery connectivity with search button, added primary variables to JS file

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,11 @@
-$("#button").click(function () {
+// Defining initial variables to use for later functions/click events
+var citySearch = $("#city-text");
+var searchBtn = $("#city-search");
+var searchHist = $("#past-searches");
+var current = $("#current-weather");
+var forecast = $("#forecast");
+
+// Testing button connectivity
+searchBtn.click(function() {
     console.log("hello!");
 })
