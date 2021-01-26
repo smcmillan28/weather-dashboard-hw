ab824caa67fafe01a348bc543ea3845b5accc48c
Sammy McMillan <74073409+smcmillan28@users.noreply.github.com>
2021-01-25 21:43:17 -0500

    completed current weather data section to write today's info to page

--- assets/script.js
+++ assets/script.js
@@ -2,14 +2,18 @@
 var citySearch = $("#city-search");
 var searchBtn = $("#search-button");
 var searchHist = $("#past-searches");
-var current = $("#current-weather");
+var currentWeather = $("#current-weather");
 var forecast = $("#forecast");
 
+//Empty array to store cities that are searched
 var pastSearch = [];
+
+// Moment.js used for date display when city is searched
+var currentTime = moment();
+var currentDay = currentTime.format("MMMM D, YYYY");
 
 // Write a function that will create a button once the search button is clicked and store that city as the button text
 // This function also stores those cities in the pastSearch array and sends that array to local storage
-
 function addCity(event) {
     event.preventDefault();
     if (citySearch.val() != "") {
@@ -26,9 +30,11 @@ function addCity(event) {
 }
 
 // Write a function that makes an AJAX call to open Weather API
-
 function weatherData() {
-    // Using Position Stack Geocoding API to get latitude and longitude
+    // Clearing previous data from page
+    currentWeather.empty();
+
+    // Using Position Stack Geocoding API to get latitude and longitude for city input
     var cityName = citySearch.val();
     var cityQueryUrl = "http://api.positionstack.com/v1/forward?access_key=1d9b73328157570ee57517d73c03e071&query=" + cityName;
 
@@ -41,14 +47,36 @@ function weatherData() {
         // Storing response object data in latitude and longitude variables
         var lat = res.data[0].latitude;
         var lon = res.data[0].longitude;
-        // Open Weather query URL
-        var weatherQueryUrl = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&exclude={part}&appid=c8d8b26d88e2efcce705725f885fb689";
+
+        // Open Weather API query URL in the form of a string
+        var weatherQueryUrl = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&units=imperial&appid=c8d8b26d88e2efcce705725f885fb689";
 
         $.ajax({
             url: weatherQueryUrl,
             method: "GET"
         }).then(function (response) {
             console.log(response);
+            // Creating div and all different HTML elements to store current weather data then appending that to the page
+            var allCurrent = $("<div>").addClass("section");
+            var currentHeader = $("<h3>").text("Current Weather and Forecast");
+            var lineBreak = $("<hr>");
+            var currentIcon = response.current.weather[0].icon;
+            var iconURL = $("<img>").attr("src", "http://openweathermap.org/img/wn/" + currentIcon + ".png");
+            var cityHeader = $("<h4>").text(cityName + " (" + currentDay + ") ");
+            cityHeader.append(iconURL);
+            var currentTemp = Math.ceil(response.current.temp);
+            var cityTemp = $("<p>").text("Temperature: " + currentTemp + " \xB0F");
+            var humidity = $("<p>").text("Humidity: " + response.current.humidity + "%");
+            var windSpeed = $("<p>").text("Wind Speed: " + response.current.wind_speed + " mph");
+            var uvInd = response.current.uvi;
+            var uvDisp = $("<p>").text("UV Index: " + uvInd);
+            allCurrent.append(currentHeader, lineBreak, cityHeader, cityTemp, humidity, windSpeed, uvDisp);
+            currentWeather.append(allCurrent);
+
+
+
+
+
         });
 
 
